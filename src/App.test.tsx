6abbc0ce2728d3e777ed97cb7198754a3fe7f0ee
Richard <richard.verheyen@gmail.com>
2023-02-21 16:01:30 +1100

    Add the buttons and their tests to allow the first bar to be changed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,53 @@
 import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
 describe("App", () => {
-  it("Renders hello world", () => {
+  it("Renders the title in a h1", () => {
     render(<App />);
     expect(screen.getByRole('heading', {
       level: 1
     })).toHaveTextContent('Progress Bars Demo');
-  })
+  });
+
+  it("Bar values cannot go under 0", () => {
+    render(<App />);
+
+    const minus25Button = screen.getByTestId('minus-25');
+
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("25%");
+    fireEvent.click(minus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("0%");
+    fireEvent.click(minus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("0%");
+  });
+
+  it("Bar values can go over 100", () => {
+    render(<App />);
+
+    const plus25Button = screen.getByTestId('plus-25');
+
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("25%");
+    fireEvent.click(plus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("50%");
+    fireEvent.click(plus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("75%");
+    fireEvent.click(plus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("100%");
+    fireEvent.click(plus25Button);
+    expect(screen.getAllByRole('label')[0]).toHaveTextContent("125%");
+  });
+
+  it("Bar values over 100 change to red", () => {
+    render(<App />);
+
+    const plus25Button = screen.getByTestId('plus-25');
+
+    fireEvent.click(plus25Button);
+    fireEvent.click(plus25Button);
+    fireEvent.click(plus25Button);
+    fireEvent.click(plus25Button);
+    expect(screen.getAllByRole('progressbar')[0]).toHaveStyle("background-color: red");
+  });
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import Bar from './Bar'
+import { useState } from "react";
+import "./App.css";
+import Bar from "./Bar";
+import Button from "@mui/material/Button";
 
 function App() {
   const [activeBar, setActiveBar] = useState(0);
-  const [state, setState] = useState([125, 50, 75]);
+  const [state, setState] = useState([25, 50, 75]);
+
+  function handleButtonClick(e: any) {
+    let value = Number(e.target.dataset.value);
+
+    // set a temporary version of the state array
+    let array = [...state];
+
+    // update the value at the activeBar's index
+    array[activeBar] += value;
+
+    // if prevent var values from going below zero
+    if (array[activeBar] < 0) {
+      array[activeBar] = 0;
+    }
+
+    setState(array);
+  }
 
   return (
     <div className="App">
       <h1>Progress Bars Demo</h1>
 
-      {
-        state.map((value, index) => (
-          <Bar key={index} value={value} />
-        ))
-      }
-      
+      {state.map((value, index) => (
+        <Bar key={index} value={value} />
+      ))}
+
+      <Button onClick={handleButtonClick} variant="contained" type="button" data-value={-25} data-testid="minus-25">-25</Button>
+      <Button onClick={handleButtonClick} variant="contained" type="button" data-value={-10} data-testid="minus-10">-10</Button>
+      <Button onClick={handleButtonClick} variant="contained" type="button" data-value={+10} data-testid="plus-10">+10</Button>
+      <Button onClick={handleButtonClick} variant="contained" type="button" data-value={+25} data-testid="plus-25">+25</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
